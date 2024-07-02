e5eae9264de75c3e4388fc31a092538aa15f13a0
Miłosz Konopka <Milosz1532@gmail.com>
2024-07-02 10:51:39 +0200

    Added new features for version 0.2.0

--- src/Translator.php
+++ src/Translator.php
@@ -3,22 +3,166 @@
 namespace TranslationLibrary;
 
 use Google\Cloud\Translate\TranslateClient;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 
-class Translator
+class TranslationSystem
 {
-    private $translate;
-    private $model;
-    private $ignoredWords;
-    private $languageCodes;
+    private $keyFilePath;
+    private $baseDir;
+    private $standardLangDir;
+    private $translateLanguages;
 
-    public function __construct($config)
+    public function __construct($keyFilePath, $baseDir, $standardLangDir, $translateLanguages)
     {
-        $this->translate = new TranslateClient([
-            'keyFilePath' => $config['keyFilePath']
-        ]);
-        $this->model = $config['model'];
-        $this->ignoredWords = $config['ignoredWords'];
-        $this->languageCodes = $config['languageCodes'];
+        $this->keyFilePath = $keyFilePath;
+        $this->baseDir = $baseDir;
+        $this->standardLangDir = $standardLangDir;
+        $this->translateLanguages = $translateLanguages;
+    }
+
+    public function translateVariables()
+    {
+        try {
+            $translate = new TranslateClient(['keyFilePath' => $this->keyFilePath]);
+            $model = 'base';
+            $ignoredWords = ["Apple", "Google", "Microsoft", "Facebook", "Discord", "Instagram", "YouTube", "Paidwork", "paidwork"];
+
+            $languageCodes = [
+                'en-us' => 'en',
+                'en-gb' => 'en',
+                'es' => 'es',
+                'de' => 'de',
+                'fr' => 'fr',
+                'it' => 'it',
+                'nl' => 'nl',
+                'da' => 'da',
+                'sv' => 'sv',
+                'pl' => 'pl',
+                'pt-br' => 'pt',
+                'pt-pt' => 'pt',
+                'ro' => 'ro',
+                'vi' => 'vi',
+                'tr' => 'tr',
+                'id' => 'id',
+                'ru' => 'ru',
+                'uk' => 'uk',
+                'th' => 'th',
+                'ur' => 'ur',
+                'ar' => 'ar',
+                'fa' => 'fa',
+                'bn' => 'bn',
+                'hi' => 'hi',
+                'pa' => 'pa',
+                'jp' => 'ja',
+                'zh-cn' => 'zh-CN',
+                'zh-tw' => 'zh-TW',
+                'ko' => 'ko',
+                'hu' => 'hu',
+                'cz' => 'cs',
+            ];
+
+            $standardFilesList = $this->getFilesListWithPath($this->standardLangDir);
+            $comparisonResults = [];
+
+            $langDirs = glob($this->baseDir . $this->translateLanguages, GLOB_ONLYDIR);
+
+            foreach ($langDirs as $langDir) {
+                $langCode = basename($langDir);
+                $langFilesList = $this->getFilesListWithPath($langDir);
+
+                foreach ($standardFilesList as $standardFile) {
+                    $standardFilePath = $this->standardLangDir . '/' . $standardFile;
+                    $langFilePath = $langDir . '/' . $standardFile;
+
+                    $standardContents = file_get_contents($standardFilePath);
+                    preg_match_all('/\$lang\[\'(.*?)\'\]\s*=\s*[\'"](.*?)[\'"]\s*;/', $standardContents, $standardMatches);
+                    $standardVariables = array_combine($standardMatches[1], $standardMatches[2]);
+
+                    if (!file_exists($langFilePath)) {
+                        if (!is_dir(dirname($langFilePath))) {
+                            mkdir(dirname($langFilePath), 0777, true);
+                        }
+                        if (!isset($comparisonResults[$langCode])) {
+                            $comparisonResults[$langCode] = [];
+                        }
+                        $comparisonResults[$langCode][$standardFile] = [
+                            'matches' => false,
+                            'missing_variables' => array_keys($standardVariables),
+                        ];
+                        $this->translateVariablesToFile($langFilePath, $langCode, $standardVariables, $translate, $model, $ignoredWords, $languageCodes);
+                        continue;
+                    }
+
+                    $langContents = file_get_contents($langFilePath);
+                    preg_match_all('/\$lang\[\'(.*?)\'\]\s*=\s*[\'"](.*?)[\'"]\s*;/', $langContents, $langMatches);
+                    $langVariables = array_combine($langMatches[1], $langMatches[2]);
+
+                    $missingVariables = array_diff_key($standardVariables, $langVariables);
+
+                    $relativePath = str_replace($this->baseDir, '', $standardFile);
+                    $relativePath = ltrim($relativePath, DIRECTORY_SEPARATOR);
+
+                    if (!empty($missingVariables)) {
+                        if (!isset($comparisonResults[$langCode])) {
+                            $comparisonResults[$langCode] = [];
+                        }
+
+                        $comparisonResults[$langCode][$relativePath] = [
+                            'matches' => empty($missingVariables),
+                            'missing_variables' => array_keys($missingVariables),
+                        ];
+                        $this->translateVariablesToFile($langFilePath, $langCode, $missingVariables, $translate, $model, $ignoredWords, $languageCodes);
+                    }
+                }
+            }
+
+            return $comparisonResults;
+        } catch (\Exception $e) {
+            // Obsługa błędów tłumaczenia lub dostępu do plików
+            echo "An error occurred: " . $e->getMessage();
+            return [];
+        }
+    }
+
+    private function translateVariablesToFile($filePath, $langCode, $variables, $translate, $model, $ignoredWords, $languageCodes)
+    {
+        $maxTextsPerRequest = 80;
+        $numTexts = count($variables);
+        $numRequests = ceil($numTexts / $maxTextsPerRequest);
+
+        $content = file_exists($filePath) ? file_get_contents($filePath) : "<?php\n\n";
+        $targetLanguageCode = $languageCodes[$langCode];
+
+        for ($i = 0; $i < $numRequests; $i++) {
+            $startIdx = $i * $maxTextsPerRequest;
+            $endIdx = min(($i + 1) * $maxTextsPerRequest, $numTexts);
+            $chunkVariables = array_slice($variables, $startIdx, $endIdx - $startIdx);
+            $chunkTexts = array_map(function($text) use ($ignoredWords) {
+                $text = $this->replaceWordsWithSpan($text, $ignoredWords);
+                $text = $this->replaceSpecialTagsWithSpan($text);
+                return $text;
+            }, array_values($chunkVariables));
+
+            $translations = $translate->translateBatch($chunkTexts, [
+                'target' => $targetLanguageCode,
+                'model' => $model,
+                'format' => "html",
+            ]);
+
+            foreach ($translations as $index => $translation) {
+                $key = array_keys($chunkVariables)[$index];
+                $translatedText = $this->removeSpanTags($translation['text']);
+                $translatedText = addslashes($translatedText);
+                $translatedText = preg_replace('/\s+(:)/', '$1', $translatedText);
+
+                $content .= "\n\$lang['$key'] = \"$translatedText\";";
+            }
+        }
+
+        file_put_contents($filePath, $content);
+
+        echo "<br><span style='color:green;'>Variables in file <b>$filePath</b> have been successfully translated to language <b>$langCode</b>.</span>";
     }
 
     private function replaceWordsWithSpan($text, $words)
@@ -39,97 +183,30 @@ class Translator
         return preg_replace('/(<[^>]*>)(-[A-Za-z0-9_-]+-)(<[^>]*>)/', '$1<span translate="no">$2</span>$3', $text);
     }
 
-    public function translateVariables($filePath, $langCode, $variables)
+    private function getFilesListWithPath($dir)
     {
-        $maxTextsPerRequest = 80;
-        $numTexts = count($variables);
-        $numRequests = ceil($numTexts / $maxTextsPerRequest);
-        $content = file_exists($filePath) ? file_get_contents($filePath) : "<?php\n\n";
-        $targetLanguageCode = $this->languageCodes[$langCode];
-
-        for ($i = 0; $i < $numRequests; $i++) {
-            $startIdx = $i * $maxTextsPerRequest;
-            $endIdx = min(($i + 1) * $maxTextsPerRequest, $numTexts);
-            $chunkVariables = array_slice($variables, $startIdx, $endIdx - $startIdx);
-            $chunkTexts = array_map(function($text) {
-                $text = $this->replaceWordsWithSpan($text, $this->ignoredWords);
-                $text = $this->replaceSpecialTagsWithSpan($text);
-                return $text;
-            }, array_values($chunkVariables));
-
-            $translations = $this->translate->translateBatch($chunkTexts, [
-                'target' => $targetLanguageCode,
-                'model' => $this->model,
-                'format' => "html",
-            ]);
-
-            foreach ($translations as $index => $translation) {
-                $key = array_keys($chunkVariables)[$index];
-                $translatedText = $this->removeSpanTags($translation['text']);
-                $translatedText = addslashes($translatedText);
-                $translatedText = preg_replace('/\s+(:)/', '$1', $translatedText);
-                $content .= "\n\$lang['$key'] = \"$translatedText\";";
-            }
+        $basePath = str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', $dir);
+        $filesList = [];
+        $this->countFilesRecursively($dir, $filesList);
+        $filesWithPath = [];
+        foreach ($filesList as $file) {
+            $relativePath = str_replace($basePath, '', $file);
+            $relativePath = ltrim($relativePath, DIRECTORY_SEPARATOR);
+            $filesWithPath[] = $relativePath;
         }
-
-        file_put_contents($filePath, $content);
-        echo "<br><span style='color:green;'>Variables in file <b>$filePath</b> have been successfully translated to language <b>$langCode</b>.</span>";
+        return $filesWithPath;
     }
 
-    public function compareLangVariables($baseDir, $standardLangDir, $translateLanguages)
+    private function countFilesRecursively($dir, &$filesList = [])
     {
-        $standardFilesList = Utils::getFilesListWithPath($standardLangDir);
-        $comparisonResults = [];
-        $langDirs = glob($baseDir . $translateLanguages, GLOB_ONLYDIR);
-
-        foreach ($langDirs as $langDir) {
-            $langCode = basename($langDir);
-            $langFilesList = Utils::getFilesListWithPath($langDir);
-
-            foreach ($standardFilesList as $standardFile) {
-                $standardFilePath = $standardLangDir . '/' . $standardFile;
-                $langFilePath = $langDir . '/' . $standardFile;
-
-                $standardContents = file_get_contents($standardFilePath);
-                preg_match_all('/\$lang\[\'(.*?)\'\]\s*=\s*[\'"](.*?)[\'"]\s*;/', $standardContents, $standardMatches);
-                $standardVariables = array_combine($standardMatches[1], $standardMatches[2]);
-
-                if (!file_exists($langFilePath)) {
-                    if (!is_dir(dirname($langFilePath))) {
-                        mkdir(dirname($langFilePath), 0777, true);
-                    }
-                    if (!isset($comparisonResults[$langCode])) {
-                        $comparisonResults[$langCode] = [];
-                    }
-                    $comparisonResults[$langCode][$standardFile] = [
-                        'matches' => false,
-                        'missing_variables' => array_keys($standardVariables),
-                    ];
-                    $this->translateVariables($langFilePath, $langCode, $standardVariables);
-                    continue;
-                }
-
-                $langContents = file_get_contents($langFilePath);
-                preg_match_all('/\$lang\[\'(.*?)\'\]\s*=\s*[\'"](.*?)[\'"]\s*;/', $langContents, $langMatches);
-                $langVariables = array_combine($langMatches[1], $langMatches[2]);
-
-                $missingVariables = array_diff_key($standardVariables, $langVariables);
-                $relativePath = str_replace($baseDir, '', $standardFile);
-                $relativePath = ltrim($relativePath, DIRECTORY_SEPARATOR);
-
-                if (!empty($missingVariables)) {
-                    if (!isset($comparisonResults[$langCode])) {
-                        $comparisonResults[$langCode] = [];
-                    }
-                    $comparisonResults[$langCode][$relativePath] = [
-                        'matches' => empty($missingVariables),
-                        'missing_variables' => array_keys($missingVariables),
-                    ];
-                    $this->translateVariables($langFilePath, $langCode, $missingVariables);
-                }
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS));
+        foreach ($iterator as $file) {
+            if ($file->isFile() && $file->getExtension() === 'php') {
+                $filesList[] = $file->getPathname();
             }
         }
-
-        return $comparisonResults;
+        return count($filesList);
     }
 }
+
+?>
